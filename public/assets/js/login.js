62fb98ae4ab6d5b234c5190a0240799b254adb2e
bram <bram@email.id>
2021-11-28 19:25:07 +0700

    Update asset of login version

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,11 +1,11 @@
 $(function() {
     $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		$("#username").focus();
-		e.preventDefault();
+			$("#login-form").delay(100).fadeIn(100);
+			$("#register-form").fadeOut(100);
+			$('#register-form-link').removeClass('active');
+			$(this).addClass('active');
+			$("#username").focus();
+			e.preventDefault();
     });
     $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
        $("#success-alert").slideUp(500);
--- views/auth/login.php
+++ views/auth/login.php
@@ -5,7 +5,8 @@
   <title>
     <?php echo $data['site_title']; ?>
   </title>
-  <link rel="stylesheet" type="text/css" href="public/assets/css/bootstrap.css">
+  <!-- <link rel="stylesheet" type="text/css" href="public/assets/css/bootstrap.css"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="public/assets/css/login.css">
 </head>
 <body>
@@ -59,8 +60,11 @@
     </div>
   </div>
   <!-- Javascript Area -->
-  <script type="text/javascript" src="public/assets/js/jQuery-2.1.3.min.js"></script>
-  <script type="text/javascript" src="public/assets/js/bootstrap.min.js"></script>
+  <!-- <script type="text/javascript" src="public/assets/js/jQuery-2.1.3.min.js"></script> -->
+  <!-- <script type="text/javascript" src="public/assets/js/bootstrap.min.js"></script> -->
+  <!-- <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script> -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script type="text/javascript" src="public/assets/js/login.js"></script>
 </body>
 </html>
